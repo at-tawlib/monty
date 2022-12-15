5a81f6099596dbfe5f2f0a6f0026b57af2792b33
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 06:25:50 +0000

    Remove unwated code

---
+++ monty.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+
+/**
+ * main - function for monty code interpreter
+ * @argc: arguments count
+ * @argv: arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
