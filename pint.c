7897eed0ffd8372d6f29105f76fa6912ad8fe4dc
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 09:57:27 +0000

    Add null check and recode

--- pint.c
+++ pint.c
@@ -9,13 +9,10 @@
  */
 void f_pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t *head;
-
-	head = *stack;
-	if (head == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", head->n);
+	printf("%d\n", (*stack)->n);
 }
