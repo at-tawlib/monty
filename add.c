6f6b38c0b077d511b574844df809a60f9179f8e2
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 11:51:48 +0000

    Add stderr

--- add.c
+++ add.c
@@ -14,7 +14,7 @@ void f_add(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
-		printf("L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
