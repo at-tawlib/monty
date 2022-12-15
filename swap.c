c7a62dd96c0a66cd6b70d0d47fbd808cbdd9a61e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 11:08:58 +0000

    Add new line to error output

--- swap.c
+++ swap.c
@@ -13,7 +13,7 @@ void f_swap(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	first = *stack;
