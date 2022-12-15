e7a317ba6b0c08856f29a7f26d583da638d8455f
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 12:19:50 +0000

    Implement f_sub function

--- monty.h
+++ monty.h
@@ -49,5 +49,6 @@ void f_pop(stack_t **stack, unsigned int line_number);
 void f_swap(stack_t **stack, unsigned int line_number);
 void f_add(stack_t **stack, unsigned int line_number);
 void f_nop(stack_t **stack, unsigned int line_number);
+void f_sub(stack_t **stack, unsigned int line_number);
 
 #endif
