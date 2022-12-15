bbfd3182bc0bbdae7f91756ea9cfd80e70c1faba
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 11:43:02 +0000

    Create add.c

---
+++ add.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * f_add - adds the top two elements of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void f_add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int sum = 0;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		printf("L%u: can't add, stack too short", line_number);
+	}
+
+	temp = *stack;
+
+	sum = (*stack)->n + (*stack)->next->n;
+	temp->next->n = sum;
+	*stack = temp->next;
+	free(temp);
+}
