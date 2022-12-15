30679d657386446662731e3a384557ea8840fa5e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 11:15:11 +0000

    Cleanup code

--- swap.c
+++ swap.c
@@ -9,21 +9,19 @@
  */
 void f_swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *first, *second, *temp;
+	stack_t *temp;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	first = *stack;
-	second = (*stack)->next;
-	temp = first;
-	first->next = second->next;
-	first->prev = second;
-	if (second->next != NULL)
-		second->next->prev = first;
-	second->next = temp;
-	second->prev = NULL;
-	*stack = second;
+	temp = (*stack)->next;
+	(*stack)->prev = temp;
+	(*stack)->next = temp->next;
+	temp->prev = NULL;
+	if (temp->next)
+		temp->next->prev = *stack;
+	temp->next = *stack;
+	*stack = temp;
 }
