68294957302bdfaecfdb5b869c1b652a79f57d23
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-15 07:57:31 +0000

    Update file

--- monty.c
+++ monty.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 
-
+stack_t **global_head;
 /**
- * main - function for monty code interpreter
+ * main - root of project
  * @argc: arguments count
  * @argv: arguments
  *
@@ -10,5 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	return (0);
+	stack_t *head;
+
+	/* if number of arguments is not 2 */
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	head = NULL;
+	global_head = &head;
+
+	read_file(argv[1], &head);
+	atexit(free_global);
+	exit(EXIT_SUCCESS);
 }
